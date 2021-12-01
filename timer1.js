9f00892da5fcd2d421e5244b5c7de764079bfa03
AlulaLeak <alula.leakemariam94@gmail.com>
2021-12-01 01:10:20 +0000

    quick name switch

--- timer1.js
+++ timer1.js
@@ -1,16 +1,16 @@
 process.argv.shift();
 process.argv.shift();
-let times = process.argv;
+let secondsArray = process.argv;
 
-times.sort(function(a, b) {
+secondsArray.sort(function(a, b) {
   return a - b;
 });
 
 let timeFr = 0;
 
-for (const item in times) {
-  times[item] *= 1000;
+for (const seconds in secondsArray) {
+  secondsArray[seconds] *= 1000;
   setTimeout(() => {
     process.stdout.write('.');
-  }, timeFr += times[item]);
+  }, timeFr += secondsArray[seconds]);
 }
